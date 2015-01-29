5c4f30b1642a22d9b9b4263aff31f58c682c2cf4
Alex <alexleone89@gmail.com>
2015-01-29 11:40:05 -0600

    Creating initial Visual Studio project

---
+++ LineParser/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace QuoteChatLineParser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(12, 12);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(103, 39);
+            this.uploadButton.TabIndex = 0;
+            this.uploadButton.Text = "Upload File";
+            this.uploadButton.UseVisualStyleBackColor = true;
+            this.uploadButton.Click += new System.EventHandler(this.uploadButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 268);
+            this.Controls.Add(this.uploadButton);
+            this.Name = "Main";
+            this.Text = "Quote Chat";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadButton;
+    }
+}
+
---
+++ LineParser/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QuoteChatLineParser
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void uploadButton_Click(object sender, EventArgs eventArgs)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                AddExtension = true,
+                Multiselect = false,
+                Filter = "SRT Files (*.srt)|*.srt"
+            };
+
+            var dialogResponse = openFileDialog.ShowDialog();
+
+            if (dialogResponse == DialogResult.OK)
+            {
+                // TODO: Read contents from file.
+                // TODO: Do something with contents from file.
+            }
+        }
+    }
+}
